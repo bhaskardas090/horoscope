86d1a9dd9a63b95e8546c569c56513029fd72020
Bhaskar Das <bhaskardas090@gmail.com>
2022-04-24 11:05:07 +0530

    get fetched from api

--- src/App.js
+++ src/App.js
@@ -5,13 +5,18 @@ import './App.css';
 import Navbar from './component/Navbar';
 import Home from './pages/Home';
 import Result from './pages/Result';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
     <div className="App">
       <Navbar />
-      <Home />
-      {/* <Result /> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/result" element={<Result />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import {useEffect, useState, useMemo} from 'react';
 import aries from '../img/aries.png';
 import aquarius from '../img/aquarius.png';
 import cancer from '../img/cancer.png';
@@ -11,13 +11,44 @@ import sagittarius from '../img/sagittarius.png';
 import gemini from '../img/gemini.png';
 import taurus from '../img/taurus.png';
 import virgo from '../img/virgo.png';
-import { useState } from 'react';
 import styles from './home.module.css';
 import { InputText } from 'primereact/inputtext';
+import { useNavigate } from 'react-router-dom'
+const axios = require("axios");
+
 const Home = () => {
+  const navigate = useNavigate();
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
-  const [zodiac, setZodiac] = useState("")
+  const [sign, setSign] = useState("gemini")
+  const [loading, setLoading] = useState(false)
+  const [result, setResult] = useState(JSON.parse(sessionStorage.getItem('result')));
+  
+  const options = {
+      method: 'POST',
+      url: 'https://sameer-kumar-aztro-v1.p.rapidapi.com/',
+      params: {sign: sign, day: 'today'},
+      headers: {
+        'X-RapidAPI-Host': 'sameer-kumar-aztro-v1.p.rapidapi.com',
+        'X-RapidAPI-Key': 'de9e63ce4cmshcb1237b75a732eap197588jsn7d2dc5322ad8'
+      }
+    };
+
+  const handleSubmit = () => {
+    setLoading(true);
+    const getdata = async() => {
+      const {data} = await axios.request(options);
+      setResult(data);
+
+      if(data){
+      sessionStorage.setItem('result', JSON.stringify(data));
+      setLoading(false)
+      navigate('/result', { replace: true })
+      }
+      sessionStorage.setItem('loading', JSON.stringify(loading));
+    }
+    getdata();
+  }
 
   return (
     <div className={styles.home}>
@@ -32,7 +63,7 @@ const Home = () => {
         </div>
       </div>
       <h1 className={styles.zodiacsHeader}>Choose Your Zodiac Sign</h1>
-      <div className={styles.zodiacsContainer}>
+      <div className={styles.zodiacsContainer} onClick={(e) => setSign(e.target.alt)}>
         <img src={aries} alt="aries" className={styles.zodiac}/>
         <img src={aquarius} alt="aquarius" className={styles.zodiac}/>
         <img src={cancer} alt="cancer" className={styles.zodiac}/>
@@ -46,7 +77,7 @@ const Home = () => {
         <img src={taurus} alt="taurus" className={styles.zodiac}/>
         <img src={virgo} alt="virgo" className={styles.zodiac}/>
       </div>
-      <button className={styles.submit}>Show My Horoscope</button>
+      <button className={styles.submit} onClick={handleSubmit}>Show My Horoscope</button>
     </div>
   )
 }
