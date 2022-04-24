a87a7b0a585ea0c0c2a17ab50ecf3762d18aae44
Bhaskar Das <bhaskardas090@gmail.com>
2022-04-24 11:35:06 +0530

    submit button style changed

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import logo from '../img/logo.svg';
 import styles from './navbar.module.css';
+// import { Link } from "react-router-dom";
+
 const Navbar = () => {
   return (
     <div className={styles.logo}>
-      <img src={logo} alt="horoscope logo" className={styles.logoImg}/>
+      {/* <Link to="/"> */}
+        <img src={logo} alt="horoscope logo" className={styles.logoImg}/>
+      {/* </Link> */}
     </div>
   )
 }
